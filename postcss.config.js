281ffce3e9ac1f708e58a30f311bec6efb6d65a7
Oliver Isenrich <oliver.isenrich@nike.com>
2017-05-23 08:53:05 -0700

    feat(index.js): Configure default exports with arrows

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  syntax: 'postcss-scss',
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('postcss-discard-comments')({
+      removeAll: true
+    }),
+    require('postcss-strip-inline-comments'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-selector-matches'),
+    require('postcss-will-change')
+  ]
+};
