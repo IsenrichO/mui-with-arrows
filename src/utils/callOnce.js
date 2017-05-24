c0ec886a0cca74ca0d4f5aedd0620dd6e26cedf3
Oliver Isenrich <oliver.isenrich@nike.com>
2017-05-24 15:21:01 -0700

    test(callOnce): Improve handling for referential issues

--- src/utils/callOnce.js
+++ src/utils/callOnce.js
@@ -5,8 +5,17 @@ const CALLED_ONCE = 'muiPrepared';
 export default function callOnce() {
   if (process.env.NODE_ENV !== 'production') {
     return (style) => {
+      if (!style[CALLED_ONCE]) {
+        throw new ReferenceError(`Error: Unable to find \
+          property ${CALLED_ONCE} of ${typeof style} \`style\``);
+      }
+
       if (style[CALLED_ONCE]) {
-        warning(false, 'Material-UI: You cannot call prepareStyles() on the same style object more than once.');
+        warning(
+          false,
+          `Material-UI: You cannot call \`prepareStyles()\`
+          on the same style object more than once.`
+        );
       }
       style[CALLED_ONCE] = true;
       return style;
