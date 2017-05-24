2cd3700961baee6cf7b526c593d54fc64737e344
Oliver Isenrich <oliver.isenrich@nike.com>
2017-05-24 16:12:34 -0700

    chore: Rebuild dist

--- lib-dist/Paper/PaperWithArrow.js
+++ lib-dist/Paper/PaperWithArrow.js
@@ -75,7 +75,21 @@ function getStyles(props, context) {
       boxShadow: paper.zDepthShadows[zDepth - 1], // No shadow for 0-depth `Paper`
       borderRadius: circle ? '50%' : rounded ? borderRadius : 0
     },
-    arrow: _Arrow.ArrowStyles
+    arrow: {
+      position: 'absolute',
+      content: '""',
+      right: 'calc(100% - 1px)',
+      // WebkitBorder: '10px solid transparent',
+      // MozBorder: '10px solid transparent',
+      // OBorder: '10px solid transparent',
+      border: '10px solid transparent',
+      // WebkitBorderRightColor: '#FFF',
+      // MozBorderRightColor: '#FFF',
+      // OBorderRightColor: '#FFF',
+      borderRightColor: '#FFF',
+      // WebkitFilter: 'drop-shadow(rgba(0, 0, 0, 0.24) -3px 0 2px)',
+      filter: 'drop-shadow(rgba(0, 0, 0, 0.24) -3px 0 2px)'
+    }
   };
 }
 
