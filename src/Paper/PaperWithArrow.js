80024dcb70e0e3648ff0a6cd82c6e3233a272f38
Oliver Isenrich <oliver.isenrich@nike.com>
2017-05-25 09:22:30 -0700

    chore

--- src/Paper/PaperWithArrow.js
+++ src/Paper/PaperWithArrow.js
@@ -34,15 +34,8 @@ function getStyles(props, context) {
     arrow: {
       position: 'absolute',
       right: 'calc(100% - 1px)',
-      WebkitBorder: '10px solid transparent',
-      MozBorder: '10px solid transparent',
-      OBorder: '10px solid transparent',
       border: '10px solid transparent',
-      WebkitBorderRightColor: '#FFFFFF',
-      MozBorderRightColor: '#FFFFFF',
-      OBorderRightColor: '#FFFFFF',
       borderRightColor: '#FFFFFF',
-      WebkitFilter: 'drop-shadow(rgba(0, 0, 0, 0.24) -3px 0 2px)',
       filter: 'drop-shadow(rgba(0, 0, 0, 0.24) -3px 0 2px)',
     },
   };
